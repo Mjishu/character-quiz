ef47603bac2687675a4fe07cff9bba2155f4bada
mjishu <128941980+Mjishu@users.noreply.github.com>
2025-02-21 16:45:04 -0500

    can select language, alphabet and then starts lesson

--- src/components/Learn.ts
+++ src/components/Learn.ts
@@ -22,12 +22,12 @@ class Learn {
         const courseParent = document.createElement('div');
         courseParent.className = 'course-parent';
         for (let i = 0; i < Object.keys(courseData).length; i++) {
-            this._lessonDisplay(courseParent, Object.keys(courseData)[i], Object.keys(Object.values(courseData)[i]).length);
+            this._lessonDisplay(courseParent, Object.keys(courseData)[i], Object.keys(Object.values(courseData)[i]).length, i);
         }
         this.content.appendChild(courseParent);
     }
 
-    _lessonDisplay(courseParent: HTMLElement, lessonName: string, courses: number) {
+    _lessonDisplay(courseParent: HTMLElement, lessonName: string, courses: number, index: number) {
         const lessonHolder = document.createElement('button');
         lessonHolder.className = 'lesson-holder';
         const lessonTitle = document.createElement('h4');
@@ -38,8 +38,35 @@ class Learn {
         courseParent.appendChild(lessonHolder);
 
         lessonHolder.addEventListener('click', () => {
-            console.log(lessonName);
+            courseParent.innerHTML = '';
+            this._chooseAlphabet(courseParent, lessonName, index);
         });
+    }
+
+    _chooseAlphabet(parent: HTMLElement, language: string, lessonNumber: number) {
+        const alphabets = Object.keys(Object.values(courseData)[lessonNumber]);
+        if (alphabets.length <= 1) {
+            console.log('just one alphabet');
+        } else {
+            this._alphabetDisplay(parent, alphabets);
+        }
+    }
+
+    _alphabetDisplay(parent: HTMLElement, alphabets: string[]) {
+        for (const alphabet of alphabets) {
+            const button = document.createElement('button');
+            button.className = 'select-alphabet-button';
+            button.innerText = alphabet;
+            button.addEventListener('click', () => {
+                parent.innerHTML = '';
+                this._startLessons(alphabet);
+            });
+            parent.appendChild(button);
+        }
+    }
+
+    _startLessons(alphabet: string) {
+        alert('starting ' + alphabet + ' lessons');
     }
 }
 
